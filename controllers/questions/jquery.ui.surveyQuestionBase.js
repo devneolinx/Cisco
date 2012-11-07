c891c89b13ef2967a416450a60fc0cdd5408e18f
devneolinx <dev@neolinx.com.np>
2012-11-07 23:04:40 +0545

    update

--- controllers/questions/jquery.ui.surveyQuestionBase.js
+++ controllers/questions/jquery.ui.surveyQuestionBase.js
@@ -50,20 +50,20 @@
             curQuestionIndex++;
             if (curQuestionIndex >= survey.questions.length) {
                 curQuestionIndex = survey.questions.length - 1;
-                filesystemHelper.getFile("result.json", function (file) {
-                    if (file) {
-                        var funcLoop = function () {
-                            if (file.isWriterAvailable()) {
-                                file.saveText("This is the first text saved using phone gap");
-                                file.readText(function (txt) { alert(txt); });
-                            }
-                            else {
-                                setTimeout(funcLoop, 100);
-                            }
-                        }
-                        funcLoop();
-                    }
-                });
+                /*filesystemHelper.getFile("result.json", function (file) {
+                if (file) {
+                var funcLoop = function () {
+                if (file.isWriterAvailable()) {
+                file.saveText("This is the first text saved using phone gap");
+                file.readText(function (txt) { alert(txt); });
+                }
+                else {
+                setTimeout(funcLoop, 100);
+                }
+                }
+                funcLoop();
+                }
+                });*/
                 alert("Congratulations, you have completed the survey");
                 this.navigateTo("thankYou", null);
             }
@@ -93,8 +93,10 @@
         },
         _resourceClicked: function (sender, e) {
             e.preventDefault();
-            window.plugins.childBrowser.showWebPage("http://google.com");
+            alert($(sender).attr("href"));
             console.log($(sender).attr("href"));
+            window.plugins.childBrowser.showWebPage("http://google.com");
+
         }
 
 
