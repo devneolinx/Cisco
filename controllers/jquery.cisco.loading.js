d4cd594106cd293410565908e61fcca219cd7135
smartmobe <dev.b.khadka@gmail.com>
2012-11-12 17:33:15 +0545

    ui improvements

---
+++ controllers/jquery.cisco.loading.js
@@ -0,0 +1,27 @@
+(function ($) {
+    $.widget("cisco.loading", $.ui.mainController, {
+        //defaul options
+        options: {
+            model: null //model needed by this controller/view            
+        },
+        //constructor
+        _create: function () {
+            $.ui.mainController.prototype._create.call(this);
+            
+        },
+        //called when widget is called with no parameter of only options after widget is created 
+        _init: function () {
+            $.ui.mainController.prototype._init.call(this);
+        },
+        //destructor
+        destroy: function () {
+            $.Widget.prototype.destroy.call(this);
+        },
+        show: function(){
+        	this.element.show();
+        },
+        hide: function(){
+        	this.element.hide();
+        }
+    });
+})(jQuery);
